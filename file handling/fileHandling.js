af6ff8817dd624f91fc149d32b8d1d26d8923166
amritrajputt <vishal179singh@gmail.com>
2025-10-23 23:23:25 +0530

    commit-all

--- file handling/fileHandling.js
+++ file handling/fileHandling.js
@@ -11,46 +11,46 @@ const fileHandling =  require ("fs")
 
 // // writing in file in asynchronous way
 
-// fileHandling.writeFile("newfile2.txt",
-//     "this file is created by fs module in asynchronous way.",
-//     (error)=>{
-//     if (error) {
-//       console.log("Something went wrong:", error);
-//     } else {
-//       console.log("File created successfully!");
-//     }
-//   })
+fileHandling.writeFile("newfile2.txt",
+    "this file is created by fs module in asynchronous way.",
+    (error)=>{
+    if (error) {
+      console.log("Something went wrong:", error);
+    } else {
+      console.log("File created successfully!");
+    }
+  })
 
 
 // // reading from a file in synchronous way
 
-// const result = fileHandling.readFileSync("notes.txt","utf-8")
-// console.log(result);
+const result = fileHandling.readFileSync("notes.txt","utf-8")
+console.log(result);
 
 
 // // reading from a file in asynchronous way
 
-// fileHandling.readFile("notes.txt","utf-8",(error,result)=>{
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log(result);
+fileHandling.readFile("notes.txt","utf-8",(error,result)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log(result);
         
-//     }
-// })
+    }
+})
 
 // // in writefile/writeFileSync if we try to change then it will be overwritten
 // // so here we use appendFile
 
-// fileHandling.appendFileSync("notes.txt",`appended text.\n`)
+fileHandling.appendFileSync("notes.txt",`appended text.\n`)
 
 
 // // for copy 1 file from another
-// fileHandling.cpSync("notes.txt","./copy.txt")
+fileHandling.cpSync("notes.txt","./copy.txt")
 
 
 // // for deleting
-// fileHandling.unlinkSync("./copy.txt")
+fileHandling.unlinkSync("./copy.txt")
 
 
 // for creating a folder
